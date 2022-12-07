c4d24d8f61c8ebfb4995a44187f02c7dbc29fe3e
Hannah Gedlaman <91993290+hged13@users.noreply.github.com>
2022-12-07 15:33:04 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,6 +8,11 @@ class Reccomender(MycroftSkill):
     @intent_file_handler('reccomender.intent')
     def handle_reccomender(self, message):
         self.speak_dialog('reccomender')
+    
+    
+    def get_user(self):
+    file = open('/home/pi/.config/mycroft/skills/NewUserCreation/log.csv', 'r')
+        df = pd.read_csv(file)
 
 
 def create_skill():
